f19ad4b0330e19b69a5115820dc50b613d76aa47
Theodeo99 <163339206+Theodeo99@users.noreply.github.com>
2025-09-09 14:04:50 +0200

    Delete mon_notebook.py

--- mon_notebook.py
+++
@@ -1,7 +0,0 @@
-# affiche_nom.py (intégrer dans mon_notebook.py ou appeler depuis main.py)
-import streamlit as st
-
-def main():
-    st.set_page_config(page_title="Osmoz", layout="wide")
-    st.title("Osmoz_1.22.ipynb")  # <-- affiche exactement ce texte
-    st.caption("Notebook source : Osmoz_1.22.ipynb")
